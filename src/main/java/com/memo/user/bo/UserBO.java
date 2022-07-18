d55a9df74f638935756bbeef21b948f202db1db6
DESKTOP-P0MDT5B\user <aaaa@naver.com>
2022-07-18 17:06:39 +0900

    초기환경 셋팅

---
+++ src/main/java/com/memo/user/bo/UserBO.java
@@ -0,0 +1,20 @@
+package com.memo.user.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.memo.user.dao.UserDAO;
+import com.memo.user.model.User;
+
+@Service
+public class UserBO {
+	
+	@Autowired
+	private UserDAO userDAO;
+	public List<User> getuser(){
+		return userDAO.selectuser();
+	}
+
+}
